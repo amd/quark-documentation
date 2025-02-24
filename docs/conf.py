36e6ad67202b34ddf0d2657a309b74cce0b308c8
Thiago Crepaldi <Thiago.FernandesCrepaldi@amd.com>
2025-02-24 17:19:45 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -146,7 +146,7 @@ language = 'en'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['include', 'api_rst', '_build', 'Thumbs.db', '.DS_Store', 'versions.md', 'license.md']
+exclude_patterns = ['include', 'api_rst', '_build', 'Thumbs.db', '.DS_Store', 'license.md']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
